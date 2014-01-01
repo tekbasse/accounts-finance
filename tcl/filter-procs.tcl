cfeaf7dd9691ab9636c9a4cbe171dca0caba2a9a
Benjamin Brink <tekbasse@yahoo.com>
2014-01-01 08:16:39 -0800

    add procs for making filters ie number series lists

--- tcl/filter-procs.tcl
+++ tcl/filter-procs.tcl
@@ -13,6 +13,21 @@ ad_proc -public acc_fin::smoothing_filter {
 } {
     Returns the data_list as a list of numbers smoothed by geometric averaging of n-terms in sequence.. For example, if filter_list contains 1 2 4, then for each data list item, the nth term will be recalculated using the geometric average of 4*nth term + 2 * (n-1) term + 1 * (n - 2). The first n-1 terms will have partial averaging, since the filter cannot extend before the first term.  
 } {
+    
+    # make some procs that generate N term filters ie number series lists.
+    # binary expansion:       (1-x)(1-2*x) ?                                                                                                         
+    # Fibonacci                                                                                                                                      
+    # power of 2 expans.               10^a(n)+1                                                                                                     
+    # 100*e/(x+1))  round(271.8/(x+1))                                                                                                               
+    # or term_count / ( x + 1/term_count )                                                                                                           
+    set invese_p2_list [list 1 1 1 1 2 2 2 3 4 6 11 144]
+    # Graph vs. actual data. and check best fit...                                                                                                   
+    set inverse_prop_list [list 272 136 91 68 54 45 39 34 30 27 25 23 21 19 18 17 16 ]
+    set inverse_p_rev_list [list 16 17 18 19 21 23 25 27 30 34 39 45 54 68 91 136 272 ]
+    set binary_expan_list [list 1 1 2 4 8 16 32 64 128 256 512 1024 2048]
+    set fibonnacci_list [list 1 1 2 3 5 8 13 21 34 55 89 144 ]
+    set sums_of_all_previous_list [list 1 2 3 6 12 24 48 96 192 384 768 1536 ]
+
     return filtered_data_list
 }
  
