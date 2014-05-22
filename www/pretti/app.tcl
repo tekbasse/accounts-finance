2d7bf73b61917b01644eb785db4b1f9113dfc941
Benjamin Brink <tekbasse@yahoo.com>
2014-05-22 09:44:32 -0700

    changed app filename to reflect new usage

--- www/pretti/app.tcl
+++ www/pretti/app.tcl
@@ -65,7 +65,7 @@ if { $form_posted } {
 
     switch -exact -- $mode {
         e {
-            ns_log Notice "pert.tcl.68:  validated for e"
+            ns_log Notice "app.tcl.68:  validated for e"
             set validated 1
             if { ![qf_is_natural_number $table_tid] } {
                 set mode "n"
@@ -75,7 +75,7 @@ if { $form_posted } {
         d {
             set validated 1
             if { ( ![qf_is_natural_number $table_tid] ) || !$delete_p } {
-                ns_log Notice "pert.tcl table_tid '${table_tid}' or delete_p $delete_p is not valid for mode d"
+                ns_log Notice "app.tcl table_tid '${table_tid}' or delete_p $delete_p is not valid for mode d"
                 set mode "p"
                 set next_mode ""
             } 
@@ -83,7 +83,7 @@ if { $form_posted } {
         t {
             set validated 1
             if { ![qf_is_natural_number $table_tid] } {
-                ns_log Notice "pert.tcl.86: table_tid '${table_tid}' is not valid for mode t"
+                ns_log Notice "app.tcl.86: table_tid '${table_tid}' is not valid for mode t"
                 set mode "p"
                 set next_mode ""
             } 
@@ -91,7 +91,7 @@ if { $form_posted } {
         c {
             set validated 1
             if { ![qf_is_natural_number $table_tid] } {
-                ns_log Notice "pert.tcl.94: table_tid '${table_tid}' is not valid for mode c"
+                ns_log Notice "app.tcl.94: table_tid '${table_tid}' is not valid for mode c"
                 lappend user_message_list "Table has not been specified."
                 set validated 0
                 set mode "p"
@@ -102,18 +102,18 @@ if { $form_posted } {
         w {
             set table_text $input_array(table_text)
             set validated 1
-            ns_log Notice "pert.tcl.105:  validated for w"
+            ns_log Notice "app.tcl.105:  validated for w"
         }
         n {
             set validated 1
-            ns_log Notice "pert.tcl.109:  validated for n"
+            ns_log Notice "app.tcl.109:  validated for n"
         }
         r {
             set validated 1
-            ns_log Notice "pert.tcl.113:  validated for r"
+            ns_log Notice "app.tcl.113:  validated for r"
         }
         default {
-            ns_log Notice "pert.tcl.116:  validated for v"
+            ns_log Notice "app.tcl.116:  validated for v"
             if { [qf_is_natural_number $table_tid] } {
                 set validated 1
                 set mode "v"
@@ -154,7 +154,7 @@ if { $form_posted } {
                 } else {
                     set table_title $input_array(table_title)
                 }
-                ns_log Notice "pert.tcl.157:  table_name '${table_name}' [string length $table_name]"
+                ns_log Notice "app.tcl.157:  table_name '${table_name}' [string length $table_name]"
                 # table_comments Comments
                 set table_comments $input_array(table_comments)
                 # table_text
@@ -164,13 +164,13 @@ if { $form_posted } {
                 set delimiter ","
                 # linebreak_char delimiter rows_count columns_count 
                 set table_text_stats [qss_txt_table_stats $table_text]
-                ns_log Notice "pert.tcl.167: table_text_stats $table_text_stats"
+                ns_log Notice "app.tcl.167: table_text_stats $table_text_stats"
                 set line_break [lindex $table_text_stats 0]
                 set delimiter [lindex $table_text_stats 1]
 
-                ns_log Notice "pert.tcl.171: table_text ${table_text}"
+                ns_log Notice "app.tcl.171: table_text ${table_text}"
                 set table_lists [qss_txt_to_tcl_list_of_lists $table_text $line_break $delimiter]
-                ns_log Notice "pert.tcl.173: set table_lists ${table_lists}"
+                ns_log Notice "app.tcl.173: set table_lists ${table_lists}"
                 # cleanup input
                 set table_lists_new [list ]
                 foreach condition_list $table_lists {
@@ -179,18 +179,18 @@ if { $form_posted } {
                         set cell_new [string trim $cell]
                         regsub -all -- {[ ][ ]*} $cell_new { } cell_new
                         lappend row_new $cell_new
-                        #ns_log Notice "pert.tcl.182:  new cell '$cell_new'"
+                        #ns_log Notice "app.tcl.182:  new cell '$cell_new'"
                     }
                     if { [llength $row_new] > 0 } {
                         lappend table_lists_new $row_new
                     }
                 }
                 set table_lists $table_lists_new
-                ns_log Notice "pert.tcl.189: : create/write table" 
-                ns_log Notice "pert.tcl.190: : llength table_lists [llength $table_lists]"
+                ns_log Notice "app.tcl.189: : create/write table" 
+                ns_log Notice "app.tcl.190: : llength table_lists [llength $table_lists]"
                 # detect table type for flags
                 set table_flags [acc_fin::pretti_type_flag $table_lists]
-                ns_log Notice "pert.tcl.193: table_flags $table_flags"
+                ns_log Notice "app.tcl.193: table_flags $table_flags"
                 if { [qf_is_natural_number $table_tid] } {
                     set table_stats [qss_table_stats $table_tid]
                     # name, title, comments, cell_count, row_count, template_id, flags, trashed, popularity, time last_modified, time created, user_id.
@@ -213,7 +213,7 @@ if { $form_posted } {
                     }
 
                 } else {
-                    ns_log Notice "pert.tcl.210: qss_table_create new table"
+                    ns_log Notice "app.tcl.210: qss_table_create new table"
                     qss_table_create $table_lists $table_name $table_title $table_comments "" $table_flags $package_id $user_id
                 }
 
@@ -225,7 +225,7 @@ if { $form_posted } {
         }
         if { $mode eq "d" } {
             #  delete.... removes context     
-            ns_log Notice "pert.tcl.222:  mode = delete"
+            ns_log Notice "app.tcl.222:  mode = delete"
             #requires table_tid
             # delete table_tid 
             if { [qf_is_natural_number $table_tid] } {
@@ -236,7 +236,7 @@ if { $form_posted } {
         }
         if { $mode eq "t" } {
             #  trash
-            ns_log Notice "pert.tcl.233:  mode = trash"
+            ns_log Notice "app.tcl.233:  mode = trash"
             #requires table_tid
             # delete table_tid 
             if { [qf_is_natural_number $table_tid] && $write_p } {
@@ -258,7 +258,7 @@ if { $form_posted } {
 }
 
 
-set menu_list [list [list Pert ""]]
+set menu_list [list [list App ""]]
 
 if { $write_p } {
     lappend menu_list [list new mode=n]
@@ -267,14 +267,14 @@ if { $write_p } {
 switch -exact -- $mode {
     e {
         #  edit...... edit/form mode of current context
-        ns_log Notice "pert.tcl.264:  mode = edit"
+        ns_log Notice "app.tcl.264:  mode = edit"
         set mode_name "#accounts-finance.edit#"
         #requires table_tid
         # make a form to edit 
         # get table from ID
 
 
-        set form_id [qf_form action pert method post id 20120531 hash_check 1]
+        set form_id [qf_form action app method post id 20120531 hash_check 1]
         
         qf_input type hidden value w name mode label ""
         
@@ -312,19 +312,19 @@ switch -exact -- $mode {
     w {
         #  save.....  (write) table_tid 
         # should already have been handled above
-        ns_log Notice "pert.tcl.309:  mode = save THIS SHOULD NOT BE CALLED."
+        ns_log Notice "app.tcl.309:  mode = save THIS SHOULD NOT BE CALLED."
         # it's called in validation section.
     }
     n {
         #  new....... creates new, blank context (form)    
-        ns_log Notice "pert.tcl.314:  mode = new"
+        ns_log Notice "app.tcl.314:  mode = new"
         set mode_name "#accounts-finance.new#"
         #requires no table_tid
         set table_text ""
 
         # make a form with no existing table_tid 
 
-        set form_id [qf_form action pert method post id 20140415 hash_check 1]
+        set form_id [qf_form action app method post id 20140415 hash_check 1]
 
         qf_input type hidden value w name mode label ""
         qf_append html "<h3>Table</h3>"
@@ -345,7 +345,7 @@ switch -exact -- $mode {
     }
     c {
         #  process... compute/process and write output as a new table, present post_calc results
-        ns_log Notice "pert.tcl.342:  mode = process"
+        ns_log Notice "app.tcl.342:  mode = process"
         set mode_name "#accounts-finance.process#"
         #requires table_tid
         # given table_tid 
@@ -354,14 +354,14 @@ switch -exact -- $mode {
     }
     r {
         #  review.... show processd output 
-        ns_log Notice "pert.tcl.351:  mode = review"
+        ns_log Notice "app.tcl.351:  mode = review"
         #requires table_tid
 
         # option not used for this app. Calcs are saved as a table. use mode v
     }
     v {
         #  view table(s) (standard, html page document/report)
-        ns_log Notice "pert.tcl.358:  mode = $mode ie. view table"
+        ns_log Notice "app.tcl.358:  mode = $mode ie. view table"
         set mode_name "#accounts-finance.view#"
         if { [qf_is_natural_number $table_tid] && $write_p } {
             lappend menu_list [list edit "table_tid=${table_tid}&mode=e"]
@@ -395,7 +395,7 @@ switch -exact -- $mode {
     default {
         # default includes v,p
         #  present...... presents a list of contexts/tables to choose from
-        ns_log Notice "pert.tcl.392:  mode = $mode ie. default"
+        ns_log Notice "app.tcl.392:  mode = $mode ie. default"
 
 
         # show tables
@@ -461,21 +461,21 @@ switch -exact -- $mode {
             # set name_link "<a\ href=\"${name}\">${name}</a>"
             set random [clock clicks]
             set random "[clock clicks][string range [expr { rand() } ] 2 end]"
-            set form_id [qf_form action pert method post id 20140420-$random hash_check 1]
+            set form_id [qf_form action app method post id 20140420-$random hash_check 1]
 
             ## if using name_link, comment out this next line:
             qf_input type submit value $select_label name "zv" class btn
 
             qf_input type hidden value $table_id name table_tid
             if { ( $admin_p || $table_user_id == $user_id ) && $trashed_p == 1 } {
-                #append active_link " \[<a href=\"pert?${table_ref_name}=${table_id}&mode=t\">${untrash_label}</a>\]"
+                #append active_link " \[<a href=\"app?${table_ref_name}=${table_id}&mode=t\">${untrash_label}</a>\]"
                 qf_input type submit value $untrash_label name "zt" class btn
             } elseif { $table_user_id == $user_id || $admin_p } {
-                #append active_link " \[<a href=\"pert?${table_ref_name}=${table_id}&mode=t\">${trash_label}</a>\]"
+                #append active_link " \[<a href=\"app?${table_ref_name}=${table_id}&mode=t\">${trash_label}</a>\]"
                 qf_input type submit value $trash_label name "zt" class btn
             } 
             if { $delete_p && $trashed_p == 1 } {
-                #append active_link " \[<a href=\"pert?${table_ref_name}=${table_id}&mode=d\">${delete_label}</a>\]"
+                #append active_link " \[<a href=\"app?${table_ref_name}=${table_id}&mode=d\">${delete_label}</a>\]"
                 qf_input type submit value $delete_label name "zd" class btn
             } 
             qf_close form_id $form_id
@@ -511,7 +511,7 @@ switch -exact -- $mode {
 
 set menu_html ""
 array unset form_input_arr
-set form_id [qf_form action pert method post id 20140417 hash_check 1]
+set form_id [qf_form action app method post id 20140417 hash_check 1]
 foreach item_list $menu_list {
     set label [lindex $item_list 0]
     set url [lindex $item_list 1]
@@ -534,7 +534,7 @@ foreach item_list $menu_list {
         set name "z${name1}${name2}"
         qf_input form_id $form_id type submit value $value name $name class btn
     }
-#    append menu_html "<a href=\"pert?${url}\">${label}</a>&nbsp;"
+#    append menu_html "<a href=\"app?${url}\">${label}</a>&nbsp;"
 }
 foreach {name value} [array get form_input_arr] {
     qf_input form_id $form_id type hidden value $value name $name label ""
@@ -548,4 +548,4 @@ foreach user_message $user_message_list {
 }
 
 set title "PRETTI ${mode_name}"
-set context [list [list pert $title] $mode_name]
+set context [list [list app $title] $mode_name]
