ef455d6065fc8e71894c0426396023bd7f7e1962
Benjamin Brink <tekbasse@yahoo.com>
2014-05-29 11:04:41 -0700

    coded process button.

--- www/pretti/app.tcl
+++ www/pretti/app.tcl
@@ -468,9 +468,25 @@ switch -exact -- $mode {
         ns_log Notice "accounts-finance/www/pretti/app.tcl.342:  mode = process"
         set mode_name "#accounts-finance.process#"
         #requires table_tid
-        # given table_tid 
-##### process pretti_..
-        
+        set table_stats_list [qss_table_stats $table_tid]
+        # name, title, comments, cell_count, row_count, template_id, flags, trashed, popularity, time last_modified, time created, user_id
+        # set table_name [lindex $table_stats_list 0]
+        # set table_title [lindex $table_stats_list 1]
+        # set table_comments [lindex $table_stats_list 2]
+        set table_flags [lindex $table_stats_list 6]
+        if { $table_flags eq "p1" } {
+#            set table_name [lindex $table_stats_list 0]
+#            set table_title [lindex $table_stats_list 1]
+#            set table_comments [lindex $table_stats_list 2]
+#            set table_template_id [lindex $table_stats_list 5]
+            set trashed_p [lindex $table_stats_list 7]
+            set trash_folder_p $trashed_p
+
+            # see lib/pretti-view-one and lib/pretti-menu1
+            # given table_tid 
+            set table_lists [qss_table_read $table_tid]
+            acc_fin::scenario_prettify $table_lists
+        }
     }
     r {
         #  review.... show processd output 
