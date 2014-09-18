eba5f6c4e35faee15789d7c3303934a6d9a4652b
Benjamin Brink <tekbasse@yahoo.com>
2014-09-18 12:39:22 -0700

    moved data generator to admin area

---
+++ www/admin/example-maker.tcl
@@ -0,0 +1,30 @@
+# generic header for static .adp pages
+
+set instance_id [ad_conn package_id]
+set user_id [ad_conn user_id]
+set read_p [permission::permission_p -party_id $user_id -object_id $instance_id -privilege read]
+if { $read_p } {
+    set write_p [permission::permission_p -party_id $user_id -object_id $instance_id -privilege write]
+    if { $write_p } {
+        set delete_p [permission::permission_p -party_id $user_id -object_id $instance_id -privilege delete]
+        if { $delete_p } {
+            set admin_p [permission::permission_p -party_id $user_id -object_id $instance_id -privilege admin]
+        } else {
+            set admin_p 0
+        }
+    } else {
+        set admin_p 0
+        set delete_p 0
+    }
+} else {
+    set write_p 0
+    set admin_p 0
+    set delete_p 0
+}
+
+if { $admin_p } {
+    acc_fin::pretti_example_maker 
+}
+set url [ad_conn package_url]
+append url "/pretti"
+ad_returnredirect $url
