8dfc3fd1b6c2f5054fb342037d04e05dc9f41948
Benjamin Brink <tekbasse@yahoo.com>
2015-03-10 21:44:42 -0700

    initial p3 doc

---
+++ www/doc/pretti-p3.adp
@@ -0,0 +1,33 @@
+<master>
+<property name="doc(title)">@title;noquote@</property>
+<property name="context">@context;noquote@</property>
+
+<h1>@title;noquote@</h1>
+<p>Definition: #accounts-finance.task_types_p3_def#</p>
+
+<h2>Example Contents field of table entry form</h2>
+<p>Here is an example of comma delimited p3 data as it appears when
+being input via the app's form:
+</p>
+@raw_html;noquote@
+
+<h2>Example resultant p3 table</h2>
+<p>Here is the same example of p3 data (as above) shown on a page once the table is saved:
+</p>
+@raw2_html;noquote@
+
+
+<h2>Reserved p3 column headings</h2>
+<p>
+Reserved p3 columns consist of task attributes. 
+Each row defines a task. 
+</p>
+<h3>Required reference</h3>
+<ul><li>
+Each <strong>type</strong> must be unique and contain only letters or numbers or underscores or dashes. 
+</li></ul>
+<h3>All reserved @type@ columns</h3>
+@p3_html;noquote@
+<p>These column headings are required for a @type@ table:
+</p>
+@p3b_html;noquote@
---
+++ www/doc/pretti-p3.tcl
@@ -0,0 +1,63 @@
+set title "PRETTI #accounts-finance.task_types_p3#"
+
+set type "p3"
+
+set context [list [list index "Documentation"] $title]
+
+set p3_text [lindex [acc_fin::example_table p30g] 2]
+set raw_html "<pre>"
+append raw_html $p3_text
+append raw_html "</pre>"
+
+
+#raw2_html
+
+set p3_list [list ]
+set p3_text_list [split $p3_text "\n"]
+foreach row $p3_text_list {
+    regsub -all -- " " $row ";" row2
+    set row_list [split $row2 ","]
+    set new_row_list [list ]
+    foreach element $row_list {
+        regsub -all -- ";" $element " " new_element
+        lappend new_row_list $new_element
+    }
+    lappend p3_list $new_row_list
+}
+
+
+
+
+set table_attribute_list [list border 1 cellspacing 0 cellpadding 3]
+
+set raw2_html [qss_list_of_lists_to_html_table $p3_list $table_attribute_list]
+
+
+
+
+
+set all_list [acc_fin::pretti_columns_list $type]
+set req_list [acc_fin::pretti_columns_list $type 1]
+set var ${type}_html
+set var2 ${type}b_html
+set table_lists [list ]
+lappend table_lists [list "#accounts-finance.columns#" "#accounts-finance.title#" "#accounts-finance.description#"]
+foreach column $all_list {
+    set row_list [list $column "#accounts-finance.${column}#" "#accounts-finance.${column}_def#"]
+    lappend table_lists $row_list
+}
+set $var [qss_list_of_lists_to_html_table $table_lists $table_attribute_list]
+set $var2 ""
+if { $type ne "p4" && $type ne "p5" && $type ne "p6" } {
+    set delimiter ""
+    foreach req $req_list {
+        append $var2 $delimiter
+        append $var2 $req
+        set delimiter ", "
+    }
+    if { $var2 ne "" } {
+        append $var2 " *"
+    }
+}
+
+
