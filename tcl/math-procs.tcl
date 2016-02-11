a96ed115ec194ad766070ca737f61eb39a16727f
Benjamin Brink <tekbasse@yahoo.com>
2016-02-10 19:10:06 -0500

    split proc into two procs after checking that qaf_interpolatep1p2_at_x is apparently not used.

--- tcl/math-procs.tcl
+++ tcl/math-procs.tcl
@@ -7,56 +7,60 @@ ad_library {
 
 namespace eval acc_fin {}
 
-ad_proc -public qaf_interpolatep1p2_at_x {
+ad_proc -public qaf_interp_line_p1p2_at_x {
     p1_x
     p1_y
     p2_x
     p2_y
     p3_x
     {avg_p "0"}
-    {method "1"}
 } {
-    Method 0 returns y value of third point (p3), given cartesion points p1(x,y), p2(x,y). if avg_p == 1, returns the average of p1_y and p2_y instead of returning an empty string. 
-    Method 1 returns y value of third point (p3) assuming p2_x > p1_y and p3_x is between p1_x and p2_x.
+    Returns y value of third point (p3), given cartesion points p1(x,y), p2(x,y). if avg_p == 1, returns the average of p1_y and p2_y instead of returning an empty string. 
     The average is useful for some point interpretations.
 } {
-    switch -- $method {
-        0 {
-            # interpolate, y=mx+b, slope = Dy/Dx = m, b = y axis intercept
-            if { $p2_x != $p1_x } {
-                # Classic math way:
-                set m [expr { ( $p2_y - $p1_y ) / ( $p2_x - $p1_x ) } ]
-                if { $p2_x != 0. } {
-                    set b [expr { $p2_y / ( $m * $p2_x ) } ]
-                } elseif { $p1_y != 0. } {
-                    set b [expr { $p1_y / ( $m * $p1_x ) } ]
-                }
-                set p3_y [expr { ( $m * $p3_x ) + $b } ]
-                # This might work, but haven't completely tested.. would be faster if it does.
-                #        set delta_x31_pct [expr { ( $p3_x - $p1_x ) / ( $p2_x - $p1_x ) } ]
-                #        set p3_y [expr { $delta_x31_pct * ( $p2_y - $p1_y ) } ]
-            } else {
-                # vertical line. 
-                if { $avg_p } {
-                    set p3_y [expr { ( $p1_y + $p2_y ) / 2. } ]
-                } else {
-                    set p3_y ""
-                }
-            }
+    # interpolate, y=mx+b, slope = Dy/Dx = m, b = y axis intercept
+    if { $p2_x != $p1_x } {
+        # Classic math way:
+        set m [expr { ( $p2_y - $p1_y ) / ( $p2_x - $p1_x ) } ]
+        if { $p2_x != 0. } {
+            set b [expr { $p2_y / ( $m * $p2_x ) } ]
+        } elseif { $p1_y != 0. } {
+            set b [expr { $p1_y / ( $m * $p1_x ) } ]
         }
-        1 {
-
-            set x_diff [expr { $p2_x - $p1_x } ]
-
-            if { $x_diff == 0 } {
-                set p3_y $p1_y
-            } else {
-                set y_diff [expr { $p2_y - $p1_y } ]
-                set x3_diff [expr { $p3_x - $p1_x } ]
-                set x_pct_diff [expr { $x3_diff / $x_diff } ]
-                set p3_y [expr { $x_pct_diff * $y_diff + $p1_y } ]
-            }
+        set p3_y [expr { ( $m * $p3_x ) + $b } ]
+        # This might work, but haven't completely tested.. would be faster if it does.
+        #        set delta_x31_pct [expr { ( $p3_x - $p1_x ) / ( $p2_x - $p1_x ) } ]
+        #        set p3_y [expr { $delta_x31_pct * ( $p2_y - $p1_y ) } ]
+    } else {
+        # vertical line. 
+        if { $avg_p } {
+            set p3_y [expr { ( $p1_y + $p2_y ) / 2. } ]
+        } else {
+            set p3_y ""
         }
+    }
+
+    return $p3_y
+}
+
+ad_proc -public qaf_interpolatep1p2_at_x {
+    p1_x
+    p1_y
+    p2_x
+    p2_y
+    p3_x
+} {
+    Returns y value of third point (p3) assuming p2_x > p1_y and p3_x is between p1_x and p2_x.
+} {
+    set x_diff [expr { $p2_x - $p1_x } ]
+
+    if { $x_diff == 0 } {
+        set p3_y $p1_y
+    } else {
+        set y_diff [expr { $p2_y - $p1_y } ]
+        set x3_diff [expr { $p3_x - $p1_x } ]
+        set x_pct_diff [expr { $x3_diff / $x_diff } ]
+        set p3_y [expr { $x_pct_diff * $y_diff + $p1_y } ]
     }
     return $p3_y
 }
