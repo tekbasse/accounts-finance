f3d00abdff490c84e3e2c14a1c4aa7401a7a4965
Benjamin Brink <tekbasse@yahoo.com>
2016-06-28 00:21:17 -0400

    removed duplicate proc, and outdated comment

--- tcl/affiliate-procs.tcl
+++ tcl/affiliate-procs.tcl
@@ -13,25 +13,5 @@ ad_library {
 
 namespace eval acc_fin {}
 
-## acs-tcl/tcl/set-operation-procs.tcl 
 
-
-# sample proc
-ad_proc -public acc_fin::pretti_equation_vars {
-} {
-    Returns a list of value triplets, where each value pair consists of 1. a variable name used in pretti custom equation feature,  2. a human legible variable equivalent, and 3. a brief definition of the variable
-} {
-    set vars_lol [list [list 0 path_len "Number of different activities in path"] \
-                      [list 1 path_len_w_coefs "Number of activities in path"] \
-                      [list 2 act_cp_ratio "Ratio of activities in path that are also in Critical Path"] \
-                      [list 3 cost_ratio "Cost ratio: cost of path / cost of project"] \
-                      [list 4 on_critical_path_p "1 if path is Critical Path, otherwise 0"] \
-                      [list 5 duration_ratio "Duration ratio: duration of path / duration of Critical Path"] \
-                      [list 6 path_counter "A path's sequence number from Critical Path based on PRETTI index"] \
-                      [list 7 act_count_median "Median count of unique activities on a path."] \
-                      [list 8 act_count_max "Max count of unqiue activities on a path."] \
-                      [list 9 paths_count "Count of all complete paths."] \
-                      [list 10 a_sig_path_p "1 if path contains at least one activity that is above the median count (act_path_count_median), otherwise 0"] ]
-    return $vars_lol
-}
-
+#  procs here
