3dbd9938a27ea1ae643b0c33f0938fd130606c38
Benjamin Brink <tekbasse@yahoo.com>
2018-08-08 17:08:42 -0700

    remove/moved procs to accounts-contacts pkg

--- tcl/math-procs.tcl
+++ tcl/math-procs.tcl
@@ -43,27 +43,6 @@ ad_proc -public qaf_extrapolate_p1p2_at_x {
     return $p3_y
 }
 
-ad_proc -public qaf_interpolatep1p2_at_x {
-    p1_x
-    p1_y
-    p2_x
-    p2_y
-    p3_x
-} {
-    Returns y value of third point (p3) assuming p2_x > p1_y and p3_x is between p1_x and p2_x.
-} {
-    set x_diff [expr { $p2_x - $p1_x } ]
-
-    if { $x_diff == 0 } {
-        set p3_y $p1_y
-    } else {
-        set y_diff [expr { $p2_y - $p1_y } ]
-        set x3_diff [expr { $p3_x - $p1_x } ]
-        set x_pct_diff [expr { $x3_diff / $x_diff } ]
-        set p3_y [expr { $x_pct_diff * $y_diff + $p1_y } ]
-    }
-    return $p3_y
-}
 
 ad_proc -public qaf_round_to_decimals {
     number
